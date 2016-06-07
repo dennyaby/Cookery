55a01c4b5e26557dd1d161ff374e3aea0189f1ce
Dennya <dennya@macbook-pro-dennya.local>
2016-06-07 14:29:49 +0300

    Initial Commit

---
+++ Cookery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cookery
+//
+//  Created by  Dennya on 07.06.16.
+//  Copyright © 2016  Dennya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Cookery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cookery
+//
+//  Created by  Dennya on 07.06.16.
+//  Copyright © 2016  Dennya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
